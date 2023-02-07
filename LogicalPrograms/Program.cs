32961d63c58457b4bac6037ddde32643f333de68
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-02-07 16:49:37 +0530

    Fibonacci Series

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Fibonacci Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Fibonacci Series is running");
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.FibonacciSeries();
+                    break;
+                        
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
